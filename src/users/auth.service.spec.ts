546e87efe9094ea3f550a55c096d349f68275eed
Akshay-Madrikar <akshaymadrikar@gmail.com>
2022-01-25 23:42:20 +0530

    added more test scenarios

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,25 +3,53 @@ import { AuthService } from './auth.service';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 
-it('create an instance of auth service', async () => {
-  //Create a fake copy of the users service
-  const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+let service: AuthService;
+let fakeUserService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  beforeEach(async () => {
+    //Create a fake copy of the users service
+    fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hash password', async () => {
+    const user = await service.signUp('asd@asd.com', '12345');
+
+    expect(user.password).not.toEqual('12345');
+
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email already in use', async (done) => {
+    fakeUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '12345' } as User]);
+
+    try {
+      await service.signUp('asd@asd.com', '12345');
+    } catch (err) {
+      done();
+    }
+  });
 });
